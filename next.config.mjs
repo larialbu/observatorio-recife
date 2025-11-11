27c74591deaf25c6c969c3a45a139919539b0b42
rodrigoacm10 <137843317+rodrigoacm10@users.noreply.github.com>
2025-11-11 10:58:43 -0300

    add: deploy config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   // Configurações específicas se necessário
   experimental: {
     // Ativar se precisar de recursos experimentais
